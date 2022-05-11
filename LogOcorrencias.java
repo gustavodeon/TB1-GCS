621860f5da1dfcca29fbd9c8e48184ca9df63c0f
Flávia Webster <75635385+flavia-webster@users.noreply.github.com>
2022-05-11 02:03:10 -0300

    Construindo

--- LogOcorrencias.java
+++ LogOcorrencias.java
@@ -1,3 +1,27 @@
 public class LogOcorrencias {
     // Método para registrar palavras proibidas, data e hora dentro de seu texto original
+
+    public static void main (String[] args){
+
+    }
+
+
+    public logOcorrencias(){
+
+    }
+
+    public get.logOcorrencias(){
+
+    }
+
+    public set.logOcorrencias(){
+
+    }
+
+    @Override
+    public String toString() {
+     
+    }
+
+
 }
