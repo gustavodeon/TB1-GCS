a6d562addd5f78ab289e0db55a62692f6609018c
Rodrigo Oliveira <rodrigo.rosa81@edu.pucrs.br>
2022-05-11 02:01:01 -0300

    iniciando classe teste

--- TestOcorrencias.java
+++ TestOcorrencias.java
@@ -1,3 +1,10 @@
 public class TestOcorrencias {
     //Classe provisária para registrar ocorrências de palavras proibidas 
+public static void main (String[]args){
+
+}
+LogOcorrencias ts = new LogOcorrencias();
+
+
+
 }
