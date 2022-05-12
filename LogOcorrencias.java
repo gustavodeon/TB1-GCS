3a468c602ca56c70e791a7379f33d477efc909ad
Flávia Webster <75635385+flavia-webster@users.noreply.github.com>
2022-05-12 11:39:58 -0300

    Acho que foi

--- LogOcorrencias.java
+++ LogOcorrencias.java
@@ -1,81 +1,30 @@
-import java.util.Date;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.*;
 
 public class LogOcorrencias {
     // Método para registrar palavras proibidas, data e hora dentro de seu texto original
   
-    private ArrayList<Log> log;
-    private static final String MyLog = null;
-    private ArrayList<LogOcorrencias> log;
-    private Usuario usuario;
-    private Date dataPostagem;
-    private String conteudo;
 
-    public void logOcorrencias(Usuario autor, String conteudo, Date dataPostagem) throws SecurityException, IOException {
+    private Usuario usuario;
+    private String dataPostagem;
+    private String conteudo;
+    private ArrayList<LogOcorrencias> array;
+
+    LogOcorrencias(Usuario autor, String conteudo, String dataPostagem) {
         this.usuario = autor;
         this.conteudo = conteudo;
         this.dataPostagem = dataPostagem;
-        log = new ArrayList<>();
-        FileHandler myHandler = new FileHandler ( " conteudo ");
-        Logger myLogger = Logger.getLogger ( LogOcorrencias.class.getName ());
-        myLogger.addHandler ( myHandler ) ;
-        myLogger.info ( "Este é um evento log ");
-
-    public void logOcorrencias(Log l) {
-        log = new ArrayList<>();
-        log.add(l);
     }
-
-    public int getQtdeOcorrenciasLog(){
-        return log.size();
-    }
-
-
-    public LogOcorrencias(){
-
-    }
-
-<<<<<<< HEAD
-    public Usuario getOcorrenciaAutor() {
-        return usuario;
-=======
-    public Log getOcorrenciaAutor(Usuario user) {
-        for (Log l:log){
-            if(log.g)
-        }
->>>>>>> 0ddcd8702019aa3f989a18919037487417a756fb
+ 
+    public void armazena (Usuario autor, String conteudo, String dataPostagem){
+        LogOcorrencias ocorrencias = new LogOcorrencias(autor, conteudo, dataPostagem);
+        array.add(ocorrencias);
     }
 
     @Override
     public String toString() {
-        return conteudo;
+        return "Usuario" + usuario + " data postagem" + dataPostagem + " conteudo" + conteudo;
+        
      
     }
 
-private class Log {
-    protected Usuario usuario;
-    protected Date dataPostagem;
-    protected String conteudo;
-    protected int idLog;
-
-public void Log (Usuario autor, String conteudo, Date dataPostagem) {
-        this.usuario = autor;
-        this.conteudo = conteudo;
-        this.dataPostagem = dataPostagem;
-        this.idLog = 
-}
-
-public Usuario getPorUsuario(){
-    return usuario;
-}
-public String getConteudo(){
-    return conteudo;
-}
-public Date getDataPost(){
-    return dataPostagem;
-}
-
-}
 }
